4867364496c3696a3b0d736ec12fc7245370afc6
xujiashuai <605162215@qq.com>
2017-07-10 17:09:33 +0800

    update

--- 备份日志.sh
+++ 备份日志.sh
@@ -5,11 +5,15 @@ src_logfile=/usr/local/tomcat-front/logs/catalina.out
 dst_name_prefix=front
 
 today=`date '+%Y-%m-%d'` 
-dst_logfile=$dst_name_prefix.$today.log 
+dst_name=$dst_name_prefix.$today.log 
 
-basepath=$(cd `dirname $0`; pwd)
-cp $src_logfile $basepath/logs/$dst_logfile
+base_path=$(cd `dirname $0`; pwd)
+dst_log_dir=$base_path/logs/
+if [ ! -d $dst_log_dir ];then 
+    mkdir $dst_log_dir
+fi 
+
+cp $src_logfile $dst_log_dir/$dst_name
 
 cat /dev/null > $src_logfile
-find $basepath/logs -mtime +6 -type f -name "$dst_name_prefix.*.log" | xargs rm -f
-
+find $dst_log_dir -mtime +6 -type f -name "$dst_name_prefix.*.log" | xargs rm -f
